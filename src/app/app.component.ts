7f352b68f7beb05cacd2fe453e015861dbb575a5
Yerko Manzel Peralta <yerko.mp22@gmail.com>
2019-08-30 20:25:54 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devops-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'project-devops-app';
+}
